9133f92d43c5345edf608edc7af4c88b8586a3ff
xdavidwu <xdavidwuph@gmail.com>
2021-08-26 05:37:30 +0000

    mcc_download_new_lang: fix filename for lang without suffix

--- mcc_download_new_lang.sh
+++ mcc_download_new_lang.sh
@@ -8,6 +8,8 @@ fi
 
 LANG_SUF=$(echo $1 | cut -d '_' -f 2 | tr -s '[:lower:]' '[:upper:]')
 
+[ -n "$LANG_SUF" ] && LANG_SUF=_$LANG_SUF
+
 #https://s3.amazonaws.com/Minecraft.Download/indexes/1.13.json
 #https://launchermeta.mojang.com/v1/packages/280eebe96a3ca45fcbc85800552cea775bc5f73c/1.14.json
 #https://launchermeta.mojang.com/v1/packages/28c11387f2c576240a31d359991bbaed28db57ab/1.15.json
@@ -17,4 +19,4 @@ HASH=$(wget https://launchermeta.mojang.com/v1/packages/e5af543d9b3ce1c063a97842
 
 mkdir -p lang
 
-wget http://resources.download.minecraft.net/$(echo $HASH | head -c 2)/$HASH -O - | jq -r 'to_entries|map("\(.key)=\(.value)")[]' > lang/$(echo $1 | cut -d '_' -f 1)_$LANG_SUF.lang
+wget http://resources.download.minecraft.net/$(echo $HASH | head -c 2)/$HASH -O - | jq -r 'to_entries|map("\(.key)=\(.value)")[]' > lang/$(echo $1 | cut -d '_' -f 1)$LANG_SUF.lang
