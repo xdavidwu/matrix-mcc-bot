f99e2b089628ec8928def92755dfda72e6a91621
xdavidwu <xdavidwuph@gmail.com>
2021-08-26 03:34:17 +0000

    mcc_download_new_lang: update assets url to 1.17

--- mcc_download_new_lang.sh
+++ mcc_download_new_lang.sh
@@ -11,8 +11,9 @@ LANG_SUF=$(echo $1 | cut -d '_' -f 2 | tr -s '[:lower:]' '[:upper:]')
 #https://s3.amazonaws.com/Minecraft.Download/indexes/1.13.json
 #https://launchermeta.mojang.com/v1/packages/280eebe96a3ca45fcbc85800552cea775bc5f73c/1.14.json
 #https://launchermeta.mojang.com/v1/packages/28c11387f2c576240a31d359991bbaed28db57ab/1.15.json
+#https://launchermeta.mojang.com/v1/packages/1e185d112e2ee017330fb83700b5eaa93fb5cdfd/1.16.json
 
-HASH=$(wget https://launchermeta.mojang.com/v1/packages/1e185d112e2ee017330fb83700b5eaa93fb5cdfd/1.16.json -O - | jq -r ".objects.\"minecraft/lang/$1.json\".hash")
+HASH=$(wget https://launchermeta.mojang.com/v1/packages/e5af543d9b3ce1c063a97842c38e50e29f961f00/1.17.json -O - | jq -r ".objects.\"minecraft/lang/$1.json\".hash")
 
 mkdir -p lang
 
